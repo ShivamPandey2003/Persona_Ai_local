2b3114753465104723d2cb73721d81ca1f8b54c1
ShivamPandey2003 <shivam.pandey@knowledgeexcel.com>
2026-06-11 11:51:16 +0530

    add loading parts

--- src/components/common/Dashboard/DataTable.tsx
+++ src/components/common/Dashboard/DataTable.tsx
@@ -164,7 +164,7 @@ export function DataTable<TData, TValue>({
               ))}
             </TableHeader>
             <TableBody>
-              {table.getRowModel().rows?.length ? (
+              {(table.getRowModel().rows?.length && !isFetching) ? (
                 table.getRowModel().rows.map((row) => (
                   <TableRow
                     key={row.id}
